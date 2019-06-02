2904f7e7164ae4766a9b460910b703db4fdde25b
zhuzhu <39504995+zhuzhu18@users.noreply.github.com>
2019-06-02 20:51:25 +0800

    Update pytorch_blitz.py

--- pytorch_blitz.py
+++ pytorch_blitz.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Macbook  2 09:43:28 2019
+Created on Macbook Jun 2 09:43:28 2019
 
 @author: zhuzhu
 """
 
 import torch
+import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1,6,3)
-        self.conv2 = nn.Conv2d(6,16,3)
+        self.conv1 = nn.Conv2d(3,6,5)
+        self.conv2 = nn.Conv2d(6,16,5)
         
-        self.fc1 = nn.Linear(16*6*6,120)
+        self.fc1 = nn.Linear(16*5*5,120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
         
@@ -39,14 +43,40 @@ class Net(nn.Module):
         return num_features
  
 net = Net()
-input = torch.randn(1,1,32,32,requires_grad=True)
-target = torch.randn(10)
-target = target.view(1,-1)
-criterion = nn.MSELoss()
 
-optimizer = optim.SGD(net.parameters(),lr=0.01)
-optimizer.zero_grad()
-out = net(input)
-loss = criterion(out, target)
-loss.backward()
-optimizer.step()
+criterion = nn.CrossEntropyLoss()
+
+optimizer = optim.SGD(net.parameters(),lr=0.001,momentum=0.9)
+
+transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+        ])
+trainset = torchvision.datasets.CIFAR10(root='./data',train=True,download=True,transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=4,shuffle=True,num_workers=2)
+testset = torchvision.datasets.CIFAR10(root='./data',train=False,download=True,transform=transform)
+testloader = torch.utils.data.DataLoader(testset,batch_size=4,shuffle=False,num_workers=2)
+
+classes = ('plane','car','bird','cat',
+           'deer','dog','frog','horse','ship','truck')
+
+def imshow(img):
+    img = img/2+0.5
+    img = img.numpy()
+    plt.imshow(np.transpose(img, (1,2,0)))
+    plt.show()
+
+for epoch in range(2):
+    running_loss = 0
+    for i, data in enumerate(trainloader):
+        imgs, labels = data
+        optimizer.zero_grad()
+        outs = net(imgs)
+        loss = criterion(outs, labels)
+        loss.backward()
+        running_loss += loss
+        optimizer.step()
+        if i % 2000 == 1999:
+            print('[%d,%5d] loss:%.3f'%(epoch+1,i+1,running_loss/2000))
+            runing_loss = 0
+print('Training finished!')
